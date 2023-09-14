31f149f152126c860ae8cf3e6aef8b24e623495b
dotz0ver <opir0oui@gmail.com>
2023-09-14 21:58:05 +0900

    [DFS] 알고리즘 수업 - 깊이 우선 탐색 2

---
+++ Baekjoon/Python/BOJ24480.py
@@ -0,0 +1,26 @@
+import sys
+input = sys.stdin.readline
+sys.setrecursionlimit(10**9)
+
+N, M, R = map(int, input().split())
+graph = [[] for _ in range(N+1)]
+visited = [0] * (N+1)
+cnt = 1
+
+for _ in range(M):
+    u, v = map(int, input().split())
+    graph[u].append(v)
+    graph[v].append(u)
+
+def dfs(t):
+    global cnt
+    visited[t] = cnt
+    graph[t].sort(reverse=True)
+    for i in graph[t]:
+        if visited[i] == 0:
+            cnt += 1
+            dfs(i)
+dfs(R)
+
+for i in range(1, N+1):
+    print(visited[i])
