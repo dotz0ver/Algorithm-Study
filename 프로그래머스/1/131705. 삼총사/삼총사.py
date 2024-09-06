416c45df4dad1f4b0ce3898d5881bf626dd7d3b2
dotz0ver <102078431+dotz0ver@users.noreply.github.com>
2024-09-06 14:18:36 +0900

    [level 1] Title: 삼총사, Time: 0.10 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/131705. 삼총사/삼총사.py
@@ -0,0 +1,9 @@
+from itertools import combinations
+
+def solution(number):
+    cnt = 0
+    for comb in combinations(number, 3):
+        if sum(comb) == 0:
+            cnt += 1
+            
+    return cnt
